96dbce7998c900dcd3d434ad07c2b5ae9b0e3a91
hyemch <blossoming33@gmail.com>
2023-09-11 20:38:00 +0900

    feat: ex02 main.cpp
    test작성, operator[] 오버로딩, std::exception

--- ex02/Array.hpp
+++ ex02/Array.hpp
@@ -2,16 +2,16 @@
 #define ARRAY_HPP
 
 #include <iostream>
-#include <stdexcept>
+#include <exception>
 
 template<class T>
 class	Array
 {
 public:
-	Array() : array(NULL), arraySize(0)
+	Array() :  arraySize(0)
 	{
-//		array = new T[0];
-	}
+		array = new T[0];
+	};
 
 	Array(unsigned int n)
 	{
@@ -19,23 +19,23 @@ public:
 		array = new T[arraySize];
 		for (int i = 0; i < arraySize; i++)
 			array[i] = T();
-	}
+	};
 
 	~Array()
 	{
 		if (array != NULL)
 			delete[] array;
-	}
+	};
 
-	Array(const Array& other)
+	Array(const Array<T>& other)
 	{
 		arraySize = other.arraySize;
 		array = new T[arraySize];
 		for (int i = 0; i <arraySize; i++)
 			array[i] = other.array[i];
-	}
+	};
 
-	Array& operator=(const Array& other)
+	Array& operator=(const Array<T>& other)
 	{
 		if (this != &other)
 		{
@@ -45,7 +45,38 @@ public:
 			for (int i = 0; i < arraySize; i++)
 				array[i] = other.array[i];
 		}
-	}
+        return *this;
+	};
+
+    class OutOfBounds : public std::exception
+    {
+    public:
+        const char * what() const throw()
+        {
+            return ("Index out of bounds");
+        };
+    };
+
+    T&  operator[](int index)
+    {
+        if (index >= arraySize || index < 0)
+            throw OutOfBounds();
+        else
+            return array[index];
+    };
+
+    const T&  operator[](int index) const
+    {
+        if (index >= arraySize || index < 0)
+            throw OutOfBounds();
+        else
+            return array[index];
+    };
+
+    int size() const
+    {
+        return arraySize;
+    };
 
 private:
 	T*	array;
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,1 +1,107 @@
 #include "Array.hpp"
+
+void    leak(void)
+{
+    system("leaks ex02");
+}
+//
+//#define MAX_VAL 750
+//int main(int, char**)
+//{
+//    atexit(leak);
+//    Array<int> numbers(MAX_VAL);
+//    int* mirror = new int[MAX_VAL];
+//    srand(time(NULL));
+//    for (int i = 0; i < MAX_VAL; i++)
+//    {
+//        const int value = rand();
+//        numbers[i] = value;
+//        mirror[i] = value;
+//    }
+//    //SCOPE
+//    {
+//        Array<int> tmp = numbers;
+//        Array<int> test(tmp);
+//    }
+//
+//    for (int i = 0; i < MAX_VAL; i++)
+//    {
+//        if (mirror[i] != numbers[i])
+//        {
+//            std::cerr << "didn't save the same value!!" << std::endl;
+//            return 1;
+//        }
+//    }
+//    try
+//    {
+//        numbers[-2] = 0;
+//    }
+//    catch(const std::exception& e)
+//    {
+//        std::cerr << e.what() << '\n';
+//    }
+//    try
+//    {
+//        numbers[MAX_VAL] = 0;
+//    }
+//    catch(const std::exception& e)
+//    {
+//        std::cerr << e.what() << '\n';
+//    }
+//
+//    for (int i = 0; i < MAX_VAL; i++)
+//    {
+//        numbers[i] = rand();
+//    }
+//    delete [] mirror;//
+//    return 0;
+//}
+
+int main(void)
+{
+    atexit(leak);
+    Array<char>  charArray;
+
+    std::cout << "charArray size: " << charArray.size() << std::endl;
+    try
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            charArray[i] = 'h';
+            std::cout << "charArray[" << i << "]: " << charArray[i] << std::endl;
+        }
+    }
+    catch (std::exception &e)
+    {
+        std::cout << e.what() << std::endl;
+    }
+
+    charArray = Array<char>(10);
+    try
+    {
+        for (int i = 0; i < 11; i++)
+        {
+            charArray[i] = 'H';
+            std::cout << "charArray[" << i << "]: " << charArray[i] << std::endl;
+        }
+    }
+    catch (std::exception &e)
+    {
+        std::cout << e.what() << std::endl;
+    }
+
+
+    Array<char>  lowerHArray(5);
+    for (int i = 0; i < 5; i++)
+    {
+        lowerHArray[i] = 'h';
+    }
+    const Array<char> copyConstructor(lowerHArray);
+    std::cout << "copy size: " << copyConstructor.size() << std::endl;
+    for (int i = 0; i < 5; i++)
+    {
+        std::cout << "copy Array[" << i << "]: " << copyConstructor[i] << std::endl;
+    }
+
+	return 0;
+}
