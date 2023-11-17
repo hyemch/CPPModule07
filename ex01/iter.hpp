e22daaae1f5c1edb814cda5326cb79452f32c9a7
hyemch <blossoming33@gmail.com>
2023-11-17 20:06:41 +0900

    [Feat] ex01 const 제거 & test

--- ex01/iter.hpp
+++ ex01/iter.hpp
@@ -3,12 +3,12 @@
 
 #include <iostream>
 
-//template <typename T, typename F>
-//void	iter(T *arr, int length, F func)
-//{
-//	for (int i = 0; i < length; i++)
-//		func(arr[i]);
-//}
+template <typename T, typename F>
+void	iter(T *arr, int length, F func)
+{
+	for (int i = 0; i < length; i++)
+		func(arr[i]);
+}
 
 template <typename T, typename F>
 void	iter(const T *arr, const int length, const F func)
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,21 +1,53 @@
 #include "iter.hpp"
 
-template <typename T>
-void	printElement(const T& element)
+//template <typename T>
+//void	printElement(const T& element)
+//{
+//		std::cout << "\033[38;5;214m" << element << "\033[0m" << std::endl;
+//}
+//
+//int	main(void)
+//{
+//	const char		charArray[] = "Good morning~*";
+//	int			intArray[] = {5, 4, 3, 2, 1};
+//	std::string lineArray[] = {"hi", "hello", "bye"};
+//
+//	iter(charArray, 15, printElement<char>);
+//	std::cout << "\033[38;5;117m---------------------------------------\033[0m" << std:: endl;
+//	iter(intArray, 5, printElement<int>);
+//	std::cout << "\033[38;5;117m---------------------------------------\033[0m" << std:: endl;
+//	iter(lineArray, 4, printElement<std::string>);
+//	return 0;
+//}
+
+class Awesome
 {
-		std::cout << "\033[38;5;214m" << element << "\033[0m" << std::endl;
+public:
+	Awesome( void ) : _n( 42 ) { return; }
+	int get( void ) const { return this->_n; }
+private:
+	int _n;
+};
+
+std::ostream & operator<<( std::ostream & o, Awesome const & rhs )
+{
+	o << rhs.get();
+	return o;
 }
 
-int	main(void)
+template< typename T >
+void print( T& x )
 {
-	const char		charArray[] = "Good morning~*";
-	int			intArray[] = {5, 4, 3, 2, 1};
-	std::string lineArray[] = {"hi", "hello", "bye"};
+	std::cout << x << std::endl;
+	return;
+}
 
-	iter(charArray, 15, printElement<char>);
-	std::cout << "\033[38;5;117m---------------------------------------\033[0m" << std:: endl;
-	iter(intArray, 5, printElement<int>);
-	std::cout << "\033[38;5;117m---------------------------------------\033[0m" << std:: endl;
-	iter(lineArray, 4, printElement<std::string>);
+int main() {
+	int tab[] = { 0, 1, 2, 3, 4 };
+	Awesome tab2[5];
+
+	iter( tab, 5, print<const int> );
+	iter( tab2, 5, print<Awesome> );
+
 	return 0;
 }
